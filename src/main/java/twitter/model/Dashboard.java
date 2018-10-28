cd00e04c6a6211a564c57d29b808aef640988847
Michu <mich1992@wp.pl>
2018-10-28 08:48:43 +0100

    Change Dashboard class, so it can use a transient Tweet author field to show in web app, and use a AuthorDoesntExistException

--- src/main/java/twitter/model/Dashboard.java
+++ src/main/java/twitter/model/Dashboard.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -16,7 +17,15 @@ public class Dashboard {
         this.entityManager = entityManager;
     }
 
-    public Tweet create(String msg, String author) {
+    /**
+     * Creates a new tweet by the existing author.
+     *
+     * @param msg    a msg
+     * @param author an existing author
+     * @return the newly created tweet
+     * @throws AuthorDoesntExistException if author doesn't exist
+     */
+    public Tweet create(String msg, String author) throws AuthorDoesntExistException {
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         TypedQuery<Author> authorQuery = entityManager.createQuery("SELECT a FROM Author a WHERE a.name = :name", Author.class);
@@ -24,14 +33,14 @@ public class Dashboard {
         Tweet tweet = new Tweet(msg);
         try {
             Author existingAuthor = authorQuery.getSingleResult();
+            tweet.setAuthor(existingAuthor.getName());
             existingAuthor.addTweets(tweet);
+            transaction.commit();
+            return tweet;
         } catch (NoResultException e) {
-            Author newAuthor = new Author(author);
-            newAuthor.addTweets(tweet);
-            entityManager.persist(newAuthor);
+            transaction.rollback();
+            throw new AuthorDoesntExistException(String.format("Author %s doesn't exist in the db.", author), e);
         }
-        transaction.commit();
-        return tweet;
     }
 
     public Stream<Tweet> load() {
@@ -41,7 +50,9 @@ public class Dashboard {
         List<Author> listOfAuthors = authorQuery.getResultList();
 
         for (Author a : listOfAuthors) {
-            listOfAllTweets.addAll(a.getTweets());
+            Collection<Tweet> tweets = a.getTweets();
+            tweets.forEach(t -> t.setAuthor(a.getName()));
+            listOfAllTweets.addAll(tweets);
         }
 
         return listOfAllTweets.stream();
