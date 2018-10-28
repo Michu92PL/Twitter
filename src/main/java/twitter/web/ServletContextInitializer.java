78be4fd052b939be1b9cf68414aa4121e4588ab2
Michu <mich1992@wp.pl>
2018-10-28 08:43:36 +0100

    Changed ServletContextInitializer so it can use a EntityManagerFactorySingleton to create EntityManager and add context for LoginServlet

--- src/main/java/twitter/web/ServletContextInitializer.java
+++ src/main/java/twitter/web/ServletContextInitializer.java
@@ -1,10 +1,10 @@
 package twitter.web;
 
+import twitter.db.EntityManagerFactorySingleton;
+import twitter.db.JPAAuthorRepository;
 import twitter.model.Dashboard;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -15,19 +15,14 @@ public class ServletContextInitializer implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent event) {
         ServletContext context = event.getServletContext();
-
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("asdf");
-        EntityManager entityManager = factory.createEntityManager();
+        EntityManager entityManager = EntityManagerFactorySingleton.getInstance().createEntityManager();
         Dashboard dashboard = new Dashboard(entityManager);
-
         context.addServlet("TwitterServlet", new TwitterServlet(dashboard)).addMapping("/twitter");
-
-
+        context.addServlet("LoginServlet", new LoginServlet(new JPAAuthorRepository(entityManager))).addMapping("/login");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
 
     }
-
 }
